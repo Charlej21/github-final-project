39ff75ffc713c9619ad50b5e75eb116268b0b2bd
Charlej21 <95437315+Charlej21@users.noreply.github.com>
2022-10-14 16:51:37 -0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: charlej21
    # Additional Authors:
-   # <your GitHub username>
+   # <your GitHub username> Charlej21
 
    # Input:
    # p, principal amount
